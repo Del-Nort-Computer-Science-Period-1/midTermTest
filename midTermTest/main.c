d676922c44dee09011dfee99968cecb67cda68e7
Lola Bulkin <lolabulkin@gmail.com>
2019-09-05 09:13:30 -0700

    lola

--- midTermTest/main.c
+++ midTermTest/main.c
@@ -13,6 +13,14 @@ int main(int argc, const char * argv[]) {
     printf("Hello, World!\n");
     //akash was here
     //colin was here
+<<<<<<< HEAD
+=======
+    //Mr M was here, here again
+    //joseph was here
+    //akash was here again
+    //mason was here
+    //Nick (IDE_Devops Team Scrum MASTer)
     //lola was here
+>>>>>>> fd1acda1ea4e38e40e6ab000339dd0a24d8357da
     return 0;
 }
