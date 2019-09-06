01c3124482da3b84c2329b5a90f85512f431b4b3
Huynh <1849853@00-63126.pusd.dom>
2019-09-06 09:20:42 -0700

    structures
    test

--- midTermTest/structures.c
+++ midTermTest/structures.c
@@ -7,3 +7,40 @@
 //
 
 #include "structures.h"
+int structures(void)
+{
+    int answer;
+    //The question
+    puts("Which of the following is NOT a data structure in C?");
+    puts("A. Array");
+    puts("B. Bob the Builder");
+    puts("C. Stack");
+    puts("D. Tree");
+    //User input
+    puts("Enter your answer from A to D \n");
+    answer = getchar();
+    switch (answer)
+    
+    {
+        case 'A':
+        case 'a':
+            puts("Sorry, you are incorrect.");
+            puts("Array is a data structure consisting of a collection of values and variables.");
+            break;
+        case 'B':
+        case 'b':
+            puts("Yes, correct!");
+            break;
+        case 'C':
+        case 'c':
+            puts("Sorry, you are incorrect.");
+            puts("Stack is a linear data structure that allows data to be added or removed from only the top.");
+            break;
+        case 'D':
+        case 'd':
+            puts("Sorry, you are incorrect.");
+            puts("Tree is a collection of nodes where each node is a data structure consisting of a value." );
+            break;
+    }
+    return 0;
+}
