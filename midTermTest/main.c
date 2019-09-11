cfb4dd4d15c956425508c4ac9ecb3cc8b63a0701
Akash Das <akash.das01@icloud.com>
2019-09-11 08:35:52 -0700

    I was here

--- midTermTest/main.c
+++ midTermTest/main.c
@@ -3,8 +3,7 @@
 //  midTermTest
 //
 //  Created by Szeto, Colin on 9/4/19.
-//  Copyright © 2019 Szeto, Colin. All rights reserved.
-//
+// Edited by Akash Das
 
 // standard libraries
 #include <stdio.h>
