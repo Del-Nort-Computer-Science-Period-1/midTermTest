ab20c7b243f2689aca8dcfb6008137f460215ba1
tianna vollrath <tianna_vollrath@yahoo.com>
2019-09-17 09:39:26 -0700

    i was here

---
+++ midTermTest/operators.h
@@ -0,0 +1,14 @@
+//
+//  operators.h
+//  midTermTest
+//
+//  Created by Vollrath, Tianna on 9/17/19.
+//  Copyright © 2019 Szeto, Colin. All rights reserved.
+//
+
+#ifndef operators_h
+#define operators_h
+
+#include <stdio.h>
+
+#endif /* operators_h */
